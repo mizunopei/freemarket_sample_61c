d36d327f2a77edc544246a49eb547fb3e0c71078
kota-es <info@k-3110.com>
2019-10-30 18:27:41 +0900

    サインアップ・ログインのルーティング指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'login', to: 'signups#login'
+  get 'signup', to: 'signups#signup'
+  get 'signup/registration', to: 'signups#registration'
+  get 'signup/sms_confirmation', to: 'signups#sms_confirmation'
+  get 'signup/address', to: 'signups#address'
+  get 'signup/card', to: 'signups#card'
+  get 'signup/complete', to: 'signups#complete'
+  
 end
