f9605b922cee8c80b6011a1b3d82e8f3697c619a
mizunopei <m.shuhei0309@gmail.com>
2019-10-31 15:36:35 +0900

    credential書き換え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "mercari61c"
+      username == "username" && password == "password"
     end
   end
 end
