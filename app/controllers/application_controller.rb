d0863a1e74315abafa302c488098a7399f60ceb3
mizunopei <m.shuhei0309@gmail.com>
2019-10-31 17:48:22 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username ==　Rails.application.credentials.username  && password == Rails.application.credentials.password
+      username == "admin" && password == "mercari61c"
     end
   end
 end
