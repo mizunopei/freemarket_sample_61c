730a9d2227dc5d24ed3fbbaea3cc0ced99d6be40
kota-es <info@k-3110.com>
2019-11-04 11:26:14 +0900

    ユーザー登録機能の実装
    ウィザード形式で基本会員情報から電話番号登録まで遷移させたのちにレコードに保存されるよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  def after_sign_out_path_for(resource)
+    '/items' 
+  end
 
   private
 
@@ -13,4 +18,12 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, :tel])
+  end
+
 end
+
+
+
--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -10,19 +10,61 @@ class SignupsController < ApplicationController
     render :index, layout: "signup"
   end
   
-  def registration
+  def new
+    @user = User.new
   end
   
   def sms_confirmation
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:last_name] = user_params[:last_name]
+    session[:first_name] = user_params[:first_name]
+    session[:last_name_kana] = user_params[:last_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    session[:birth_day] = birthday_join
+    @user = User.new
   end
 
-  def address
+  def create
+    @user= User.new(
+      nickname: session[:nickname], 
+      email: session[:email], 
+      password: session[:password], 
+      password_confirmation: session[:password_confirmation], 
+      last_name: session[:last_name], 
+      first_name: session[:first_name], 
+      last_name_kana: session[:last_name_kana], 
+      first_name_kana: session[:first_name_kana], 
+      birth_day: session[:birth_day], 
+      tel: user_params[:tel]
+      )
+    if @user.save
+      sign_in User.find(@user.id) unless user_signed_in?
+      redirect_to new_address_path
+    end
   end
 
-  def card
+  private
+
+  def birthday_join
+    # パラメータ取得
+    date_year = params[:user]["birth_day(1i)"] 
+    date_month = params[:user]["birth_day(2i)"]
+    date_day = params[:user]["birth_day(3i)"]
+    # ブランク時のエラー回避のため、ブランクだったら何もしない
+    # if date["birth_day(1i)"].empty? && date["birth_day(2i)"].empty? && date["birth_day(3i)"].empty?
+    #   return
+    # end
+
+    # 年月日別々できたものを結合して新しいDate型変数を作って返す
+    Date.new(date_year.to_i, date_month.to_i, date_day.to_i)
+
   end
 
-  def complete
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, :tel)
   end
   
 end
