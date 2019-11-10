f4715a66fa0444249918a9c055fc44bb3b05fe17
mizunopei <m.shuhei0309@gmail.com>
2019-11-10 13:53:15 +0900

    微修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_095404) do
+ActiveRecord::Schema.define(version: 2019_11_07_070119) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2019_11_05_095404) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "d_last_name", null: false
     t.string "d_first_name", null: false
@@ -48,9 +47,8 @@ ActiveRecord::Schema.define(version: 2019_11_05_095404) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["prefecture_id"], name: "index_items_on_prefecture_id"
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -107,5 +105,4 @@ ActiveRecord::Schema.define(version: 2019_11_05_095404) do
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
   add_foreign_key "sns_credentials", "users"
-
 end
