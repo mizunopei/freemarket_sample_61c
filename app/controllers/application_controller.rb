e0373f2961b0eb9e813d7e0aedeaa4aea425fbc4
mizunopei <m.shuhei0309@gmail.com>
2019-11-10 13:49:23 +0900

    itemコントローラー作成、編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,14 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
-  
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == "admin" && password == "mercari61c"
     end
   end
+
+
+
 end
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
     render :new, layout: "signup"
   end
+
+  def create
+    @item= Item.new(item_params)
+    if @item.save
+      redirect_to action: "index"
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :condition, :d_burden, :d_way, :d_date,:prefecture_id, :price,images: [])
+  end
+
 end
 
