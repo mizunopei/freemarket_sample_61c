b59941d3b094399983fb77c179e281c6068c5b3b
mizunopei <m.shuhei0309@gmail.com>
2019-11-12 15:51:43 +0900

    Itemモデルにuser_id追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_060457) do
+ActiveRecord::Schema.define(version: 2019_11_12_064931) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_060457) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["prefecture_id"], name: "index_items_on_prefecture_id"
   end
 
