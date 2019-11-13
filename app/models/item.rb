7d1e28652b6bf33a1541eeb16fcc4d03495d0050
mizunopei <m.shuhei0309@gmail.com>
2019-11-13 13:44:49 +0900

    Item、Categoryモデルアソシエージョン定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many_attached :images
   belongs_to :user
+  belongs_to :category
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
