b761ab0417f50f77d63e894aae7788e42685f4c0
anraku-ryutaro <anrakuryutaro@gmail.com>
2019-11-14 11:54:27 +0900

    image.js修正

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,7 +1,7 @@
 $(function(){
- $('.is-higher-height img').click(function(){
-  var $thisImg = $(this).attr('src');
-  var $thisAlt = $(this).attr('alt');
-  $('.owl-lazy img').attr({src:$thisImg,alt:$thisAlt});
+  $('.is-higher-height img').click(function(){
+    var thisImg = $(this).attr('src');
+    var thisAlt = $(this).attr('alt');
+  $('.owl-lazy img').attr({src: thisImg,alt: thisAlt});
  });
 });
