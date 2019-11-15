8c7d2e4a8f377b72eeafe01e6c447a38dcb389e4
mizunopei <m.shuhei0309@gmail.com>
2019-11-15 18:20:26 +0900

    items/new,show編集

---
+++ app/assets/javascripts/item/edit.js
@@ -0,0 +1,50 @@
+$(function(){
+  var images = [];
+  var inputs =[];
+  if (document.URL.match(/item/) && document.URL.match(/edit/)){
+
+
+// //要素の削除
+$(document).on("click", ".delete-button", function(e){
+  var delete_image = $(this).parent();
+  delete_image.remove();
+  console.log(delete_image)
+  var num = delete_image.data("image");
+  console.log(num)
+   $.each(inputs, function(index, input){
+     if ((num) == delete_image.data("image")){
+      $(this).remove();
+      console.log(this)
+      delete_image.remove();
+      console.log(delete_image)
+     
+  //要素のリセット
+      images.splice(num, 1);
+      inputs.splice(num, 1);
+      if(inputs.length ==0) {
+        $("#item_images").attr({
+          "data-image": 0
+        })
+      }
+   };
+   
+  })
+});
+
+
+  //販売価格などの表示
+    var price = $(".input-price").val();
+    var fee = Math.floor(price * 0.1)
+    var profit = price - fee
+
+    if (300 <= price && price<=9999999){
+      $("#display-fee").text("¥"+(Number(fee).toLocaleString()))
+      $("#display-profit").text("¥"+(Number(profit).toLocaleString()))
+    }
+
+    else{
+      $("#display-fee").text("-")
+      $("#display-profit").text("−")
+    }
+}
+})
