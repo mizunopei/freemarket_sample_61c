8d79ee8b0498b047bde48bc4618a3acb9418e3b5
mizunopei <m.shuhei0309@gmail.com>
2019-11-15 18:21:22 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   has_many_attached :images
+  
   belongs_to :user
   belongs_to :category
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-
   validates :name, presence: true, length:{maximum: 40}
   validates :introduction, presence: true, length:{maximum: 1000}
   validates :condition, presence: true
@@ -14,5 +14,6 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :d_date, presence: true
   validates :price, numericality:{only_integer:true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"300〜9,999,999の値で入力してください" }
+
 
 end
